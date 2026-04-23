938d3bedf0857120f73b198758ab65e8a852113b
ilanc <ilanc@FIRSTOFALL>
2026-04-23 10:23:48 +0200

    huh

--- Game/include/Bullet.h
+++ Game/include/Bullet.h
@@ -1,11 +1,69 @@
 #pragma once
 #include "Core/Component.h"
-#include "InputModule.h"
-
-class Bullet {
-
+#include "Core/Scene.h"
+#include "Player.h"
+#include <cmath>
+#include <iostream>
+class Bullet : public Component
+{
 public:
-	Bullet(){}
-private:
+    GameObject* player = nullptr;
+    Maths::Vector2f velocity;
+    bool active = false;
 
+    void Activate(const Maths::Vector2f& pos, const Maths::Vector2f& vel)
+    {
+        active = true;
+        velocity = vel;
+        GetOwner()->SetPosition(pos);
+    }
+
+    void Deactivate()
+    {
+        active = false;
+        GetOwner()->SetPosition(Maths::Vector2f(-1000.f, -1000.f));
+    }
+
+    void Update(float _delta_time) override
+    {
+        if (!active) return;
+
+        auto pos = GetOwner()->GetPosition();
+        pos = pos + velocity * _delta_time;
+        GetOwner()->SetPosition(pos);
+
+        //COLLISION CHECK
+        Scene* scene = GetOwner()->GetScene();
+
+        for (auto& obj : scene->GetGameObjects()) // adapte si ton engine a un autre système
+        {
+            if (obj->GetName() == "Player")
+            {
+                Player* playerComp = obj->GetComponent<Player>();
+                if ((playerComp))
+                {
+                    Maths::Vector2f diff = pos - playerComp->GetPosition();
+                    float dist2 = diff.x * diff.x + diff.y * diff.y;
+
+                    float radius = 15.f + 3.f;
+
+                    if (dist2 < radius * radius)
+                    {
+                        std::cout << "HIT PLAYER!\n";
+
+                        playerComp->TakeDamage();
+
+                        Deactivate();
+                        return;
+                    }
+                }
+            }
+        }
+
+        // hors écran
+        if (pos.x < -100 || pos.x > 2000 || pos.y < -100 || pos.y > 2000)
+        {
+            Deactivate();
+        }
+    }
 };
