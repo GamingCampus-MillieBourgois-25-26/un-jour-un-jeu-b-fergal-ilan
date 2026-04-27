3fa14e0cdc297d7556f12813872f69eea95bdc90
fertigi00 <fcomte@gaming.tech>
2026-04-27 10:36:07 +0200

    ahhhhhhhh

--- Game/Scenes/matche 3.h
+++ Game/Scenes/matche 3.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#include "Assets/Texture.h"
+#include "Components/RectangleShapeRenderer.h"
+#include "Components/SpriteRenderer.h"
+#include "Components/SquareCollider.h"
+#include "Core/Component.h"
+#include "Core/GameObject.h"
+#include "Core/Scene.h"
+#include "Modules/AssetsModule.h"
+class Match3 final : public Scene
+{
+public:
+	Match3() : Scene("Match3Scene")
+	{
+		GameObject* player = CreateDummyGameObject("Player", 200.f, sf::Color::Cyan);
+
+		GameObject* enemy = CreateDummyGameObject("Enemy", 100.f, sf::Color::Blue);
+
+		/*GameObject* enemy2 = CreateDummyGameObject("Enemy2", 0.f, sf::Color::Yellow);*/
+
+		AssetsModule* assets_module = Engine::GetInstance()->GetModuleManager()->GetModule<AssetsModule>();
+		/*Texture* texture = assets_module->LoadAsset<Texture>("logo.png");
+
+		player->CreateComponent<SpriteRenderer>(texture);*/
+	}
+	GameObject* CreateDummyGameObject(const std::string& _name, const float _position, const sf::Color _color)
+	{
+		GameObject* game_object = CreateGameObject(_name);
+		game_object->SetPosition(Maths::Vector2f(_position, _position));
+
+		SquareCollider* square_collider = game_object->CreateComponent<SquareCollider>();
+		square_collider->SetWidth(10.f);
+		square_collider->SetHeight(10.f);
+
+		RectangleShapeRenderer* shape_renderer = game_object->CreateComponent<RectangleShapeRenderer>();
+		shape_renderer->SetColor(_color);
+		shape_renderer->SetSize(Maths::Vector2f(40.f, 40.f));
+
+		return game_object;
+	}
+};
